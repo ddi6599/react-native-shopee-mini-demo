03e8c9ca6cf4d5fd6dd939d68dd8a07cb0403994
ddi <ddili6599@gmail.com>
2018-12-26 10:09:23 +0800

    mine

--- src/screens/MineScreen.js
+++ src/screens/MineScreen.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, StyleSheet, Button, View, AsyncStorage } from 'react-native';
+import { AppRegistry, StyleSheet, View, AsyncStorage, ScrollView } from 'react-native';
+import { Platform } from "react-native";
+import {
+  Button,
+  Icon,
+  ListItem,
+  Text,
+  Badge,
+  Left,
+  Right,
+  Body,
+  Switch,
+  Radio,
+  Separator
+} from "native-base";
+import HeaderScreen from '../components/header'
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFF"
+  },
+  text: {
+    alignSelf: "center",
+    marginBottom: 7
+  },
+  mb: {
+    marginBottom: 15
+  }
+});
 
 export default class MineScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleText: "个人中心",
-      bodyText: ''
+
     };
   }
 
@@ -17,33 +43,143 @@ export default class MineScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={styles.baseText}>
-          <Text style={styles.titleText}>
-            {this.state.titleText}{'\n'}{'\n'}
-          </Text>
-          <Text numberOfLines={5}>
-            {this.state.bodyText}
-          </Text>
-        </Text>
-        <Button
-          title="退出登录"
-          onPress={() => this._signOutAsync()}
-        />
+      <View style={{ flex: 1}}>
+        <HeaderScreen options={{
+          hideLeft: true,
+          title: '个人中心',
+          bg: {backgroundColor: '#f40'},
+          color: {color: 'white'}
+        }} />
+        <ScrollView>
+          <Separator bordered noTopBorder />
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#FD3C2D" }}>
+                <Icon active name="notifications" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>消息订阅</Text>
+            </Body>
+            <Right>
+              <Switch value={false} onTintColor="#50B948" />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="bluetooth" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>Bluetooth</Text>
+            </Body>
+            <Right>
+              <Text>On</Text>
+              {Platform.OS === "ios" && <Icon active name="arrow-forward" />}
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#4CDA64" }}>
+                <Icon active name="phone-portrait" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>Mobile Data</Text>
+            </Body>
+            <Right>
+              <Radio selected />
+            </Right>
+          </ListItem>
+
+          <Separator bordered />
+
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#FD3C2D" }}>
+                <Icon active name="notifications" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>Notifications</Text>
+            </Body>
+            <Right>
+              {Platform.OS === "ios" && <Icon active name="arrow-forward" />}
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#8F8E93" }}>
+                <Icon active name="switch" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>Control Center</Text>
+            </Body>
+            <Right>
+              {Platform.OS === "ios" && <Icon active name="arrow-forward" />}
+            </Right>
+          </ListItem>
+          <ListItem icon last>
+            <Left>
+              <Button style={{ backgroundColor: "#5855D6" }}>
+                <Icon active name="moon" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>Do Not Disturb</Text>
+            </Body>
+            <Right>
+              <Text>Yes</Text>
+            </Right>
+          </ListItem>
+
+          <Separator bordered />
+
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#4CDA64" }}>
+                <Icon name="arrow-dropdown" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>通讯录</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#8F8E93" }}>
+                <Icon active name="cog" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>版本更新</Text>
+            </Body>
+            <Right>
+              <Badge style={{ backgroundColor: "#FD3C2D" }}>
+                <Text>2</Text>
+              </Badge>
+            </Right>
+          </ListItem>
+          <ListItem last icon onPress={() => this._signOutAsync()}>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="hand" />
+              </Button>
+            </Left>
+            <Body>
+            <Text>退出登录</Text>
+            </Body>
+            <Right>
+              {Platform.OS === "ios" && <Icon active name="arrow-forward" />}
+            </Right>
+          </ListItem>
+        </ScrollView>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  baseText: {
-    fontFamily: '',
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('MineScreen', () => MineScreen);
